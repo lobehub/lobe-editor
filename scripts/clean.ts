974a54d2fb5ecb1882400c788f439155ba2e5878
canisminor1990 <i@canisminor.cc>
2025-08-18 21:53:02 +0800

    🐛 fix: Fix build

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -9,7 +9,7 @@ const clean = async (filename: string) => {
   const jsPath = resolve(root, filename + '.js');
   const dtsPath = resolve(root, filename + '.d.ts');
   if (existsSync(jsPath)) unlinkSync(jsPath);
-  if (dtsPath) unlinkSync(dtsPath);
+  if (existsSync(dtsPath)) unlinkSync(dtsPath);
 };
 
 for (const pkg of packages) {
