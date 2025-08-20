fbfbf68686397f0986e4b2070b1c546d3ddcb691
ilimei <hugehardzhang@gmail.com>
2025-08-20 23:09:19 +0800

    ✨ feat:  Add support for link text editing in the editor

--- src/plugins/link/react/style.ts
+++ src/plugins/link/react/style.ts
@@ -1,6 +1,6 @@
 import { createStyles } from 'antd-style';
 
-export const useStyles = createStyles(({ css }) => {
+export const useStyles = createStyles(({ css, token }) => {
   const position = css`
     position: absolute;
     z-index: 999;
@@ -9,7 +9,19 @@ export const useStyles = createStyles(({ css }) => {
   `;
 
   return {
-    editor_linkEdit: position,
+    editor_linkEdit: css`
+      position: absolute;
+      z-index: 999;
+      inset-block-start: -9999px;
+      inset-inline-start: -9999px;
+
+      padding: 10px;
+      border: ${token.colorInfoBorder};
+      border-radius: ${token.borderRadiusLG}px;
+
+      background: ${token.colorBgContainer};
+      box-shadow: ${token.boxShadow};
+    `,
 
     editor_linkPlugin: position,
 
