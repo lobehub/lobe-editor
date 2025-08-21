754eb6fc36079c097c1ee8d08e68436af6571963
ilimei <hugehardzhang@gmail.com>
2025-08-21 23:40:20 +0800

    ✨ feat: Support tasklist

--- src/plugins/list/react/style.ts
+++ src/plugins/list/react/style.ts
@@ -1,7 +1,7 @@
 import { createStyles } from 'antd-style';
 
 export const useStyles = createStyles(
-  ({ css }) => css`
+  ({ css, token }) => css`
     .editor_listUnordered&,
     .editor_listOrdered& {
       margin-block: calc(var(--lobe-markdown-margin-multiple) * 0.5em);
@@ -26,7 +26,7 @@ export const useStyles = createStyles(
     .editor_listUnordered& {
       list-style-type: none;
 
-      > .editor_listItem:not(:has(ul)) {
+      > .editor_listItem:not(:has(ul)):not([role='checkbox']) {
         &::before {
           content: '-';
           display: inline-block;
@@ -57,6 +57,64 @@ export const useStyles = createStyles(
       .editor_listOrdered {
         margin: 0;
       }
+    }
+
+    [role='checkbox'] {
+      position: relative;
+    }
+
+    .editor_listItemChecked::before,
+    .editor_listItemUnchecked::before {
+      border: 1px solid ${token.colorInfoBorder};
+      border-radius: 2px;
+    }
+
+    .editor_listItemChecked::before {
+      background: ${token.colorInfoActive};
+    }
+
+    .editor_listItemChecked:not(:has(ul))::after {
+      cursor: pointer;
+      content: '';
+
+      position: absolute;
+      inset-block-start: 50%;
+      inset-inline-start: 0.2em;
+      transform: rotate(45deg) scale(1) translate(-50%, -50%);
+
+      display: block;
+
+      width: 0.3em;
+      height: 0.6em;
+      margin-inline: -1.2em 0.5em;
+      border: 2px solid ${token.colorPrimary};
+      border-block-start: 0;
+      border-inline-start: 0;
+      opacity: 0.5;
+      background-size: cover;
+
+      transition: all 0.2s cubic-bezier(0.12, 0.4, 0.29, 1.46) 0.1s;
+    }
+
+    .editor_listItemUnchecked:not(:has(ul))::before,
+    .editor_listItemChecked:not(:has(ul))::before {
+      cursor: pointer;
+      content: '';
+
+      position: absolute;
+      inset-block-start: 50%;
+      inset-inline-start: 0;
+      transform: translateY(-50%);
+
+      display: inline-block;
+      display: block;
+
+      width: 0.9em;
+      height: 0.9em;
+      margin-inline: -1.2em 0.5em;
+
+      opacity: 0.5;
+      background-size: cover;
     }
 
     .editor_listItemNested {
