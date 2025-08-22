81dc5994db7ec741db27a77075d5dbfda86681c0
ilimei <hugehardzhang@gmail.com>
2025-08-22 23:48:39 +0800

    ✨ feat: Customize breakLine to support cursor placement

--- src/plugins/common/node/ElementDOMSlot.ts
+++ src/plugins/common/node/ElementDOMSlot.ts
@@ -1,13 +1,18 @@
-import { ElementDOMSlot } from 'lexical';
+import { CAN_USE_DOM, mergeRegister } from '@lexical/utils';
+import {
+  COMMAND_PRIORITY_HIGH,
+  ElementDOMSlot,
+  LexicalEditor,
+  SELECTION_CHANGE_COMMAND,
+} from 'lexical';
 import { LexicalPrivateDOM } from 'lexical/LexicalNode';
 
-// @ts-ignore
-export class MElementDOMSlot extends ElementDOMSlot {
-  override setManagedLineBreak(lineBreakType: null | 'empty' | 'line-break' | 'decorator') {
-    super.setManagedLineBreak(lineBreakType);
-  }
+import { $getNodeFromDOMNode } from '@/editor-kernel/utils';
 
-  override insertManagedLineBreak(webkitHack: boolean): void {
+export function patchBreakLine() {
+  // 插入一个可以容纳光标的 breakline
+
+  ElementDOMSlot.prototype.insertManagedLineBreak = function (webkitHack: boolean) {
     const prevBreak = this.getManagedLineBreak();
     if (prevBreak) {
       if (webkitHack === (prevBreak.nodeName === 'IMG')) {
@@ -17,7 +22,11 @@ export class MElementDOMSlot extends ElementDOMSlot {
     }
     const element: HTMLElement & LexicalPrivateDOM = this.element;
     const before = this.before;
-    const br = document.createElement('br');
+    const br = document.createElement('span');
+    // eslint-disable-next-line unicorn/prefer-dom-node-dataset
+    br.setAttribute('data-lexical-linebreak', 'true');
+    br.innerHTML = '&#xFEFF;<br>';
+    // br.append(document.createElement('br'));
     // eslint-disable-next-line unicorn/prefer-modern-dom-apis
     element.insertBefore(br, before);
     if (webkitHack) {
@@ -31,7 +40,43 @@ export class MElementDOMSlot extends ElementDOMSlot {
       element.insertBefore(img, br);
       element.__lexicalLineBreak = img;
     } else {
+      // @ts-expect-error not error
       element.__lexicalLineBreak = br;
     }
+  };
+}
+
+function getElement(node: Node) {
+  if (node.nodeType === Node.ELEMENT_NODE) {
+    return node as HTMLElement;
   }
+  return node.parentElement;
+}
+
+export function registerBreakLineClick(editor: LexicalEditor) {
+  return mergeRegister(
+    editor.registerCommand(
+      SELECTION_CHANGE_COMMAND,
+      () => {
+        if (CAN_USE_DOM) {
+          const sel = document.getSelection();
+          if (sel?.anchorNode) {
+            const node = getElement(sel.anchorNode);
+            if (node?.hasAttribute('data-lexical-linebreak') && node.parentElement) {
+              const p = node.parentElement;
+              editor.update(() => {
+                const elem = $getNodeFromDOMNode(p, editor);
+                if (elem) {
+                  elem.selectEnd();
+                }
+              });
+              return true;
+            }
+          }
+        }
+        return false;
+      },
+      COMMAND_PRIORITY_HIGH,
+    ),
+  );
 }
--- src/plugins/common/plugin/index.ts
+++ src/plugins/common/plugin/index.ts
@@ -20,8 +20,11 @@ import { IEditorKernel, IEditorPlugin, IEditorPluginConstructor } from '@/types'
 import { registerCommands } from '../command';
 import JSONDataSource from '../data-source/json-data-source';
 import TextDataSource from '../data-source/text-data-source';
+import { patchBreakLine, registerBreakLineClick } from '../node/ElementDOMSlot';
 import { createBlockNode } from '../utils';
 import { registerHeaderBackspace, registerRichKeydown } from './register';
+
+patchBreakLine();
 
 export interface CommonPluginOptions {
   theme?: {
@@ -258,6 +261,7 @@ export const CommonPlugin: IEditorPluginConstructor<CommonPluginOptions> = class
       registerHeaderBackspace(editor),
       registerRichKeydown(editor, this.kernel),
       registerCommands(editor),
+      registerBreakLineClick(editor),
     );
   }
 
