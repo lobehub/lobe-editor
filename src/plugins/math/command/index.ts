def5fdea5a820a32463477c4548d74c1be1f08ed
ilimei <hugehardzhang@gmail.com>
2025-08-23 23:48:44 +0800

    ✨ feat: Improve math formula editing functionality

--- src/plugins/math/command/index.ts
+++ src/plugins/math/command/index.ts
@@ -1,6 +1,7 @@
 import { $wrapNodeInElement, mergeRegister } from '@lexical/utils';
 import {
   $createParagraphNode,
+  $getNodeByKey,
   $insertNodes,
   $isRootOrShadowRoot,
   COMMAND_PRIORITY_HIGH,
@@ -8,11 +9,21 @@ import {
   createCommand,
 } from 'lexical';
 
-import { $createMathInlineNode } from '../node/index';
+import { $createMathInlineNode, MathInlineNode } from '../node/index';
 
 export const INSERT_MATH_COMMAND = createCommand<{
   code: string;
 }>('INSERT_MATH_COMMAND');
+
+export const UPDATE_MATH_COMMAND = createCommand<{
+  code: string;
+  key: string;
+}>('UPDATE_MATH_COMMAND');
+
+export const SELECT_MATH_SIDE_COMMAND = createCommand<{
+  key: string;
+  prev?: boolean;
+}>('SELECT_MATH_SIDE_COMMAND');
 
 export function registerMathCommand(editor: LexicalEditor) {
   return mergeRegister(
@@ -25,6 +36,39 @@ export function registerMathCommand(editor: LexicalEditor) {
           $insertNodes([mathNode]); // Insert a zero-width space to ensure the image is not the last child
           if ($isRootOrShadowRoot(mathNode.getParentOrThrow())) {
             $wrapNodeInElement(mathNode, $createParagraphNode).selectEnd();
+          }
+        });
+        return true;
+      },
+      COMMAND_PRIORITY_HIGH, // Priority
+    ),
+    editor.registerCommand(
+      UPDATE_MATH_COMMAND,
+      (payload) => {
+        const { code, key } = payload;
+        editor.update(() => {
+          const mathCode = $getNodeByKey<MathInlineNode>(key);
+          if (mathCode) {
+            mathCode.updateCode(code);
+            mathCode.selectNext();
+          }
+        });
+        return true;
+      },
+      COMMAND_PRIORITY_HIGH, // Priority
+    ),
+    editor.registerCommand(
+      SELECT_MATH_SIDE_COMMAND,
+      (payload) => {
+        const { key, prev } = payload;
+        editor.update(() => {
+          const mathNode = $getNodeByKey<MathInlineNode>(key);
+          if (mathNode) {
+            if (prev) {
+              mathNode.selectPrevious();
+            } else {
+              mathNode.selectNext();
+            }
           }
         });
         return true;
