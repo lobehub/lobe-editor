03bdbb67d745d676ce349e0ea6fbf75f9d80c5fb
ilimei <hugehardzhang@gmail.com>
2025-10-25 15:17:04 +0800

    ✨ feat: add codemirror plugin

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './editor-kernel/react';
 export * from './plugins/auto-complete';
 export * from './plugins/code';
 export * from './plugins/codeblock';
+export * from './plugins/codemirror-block';
 export * from './plugins/common';
 export * from './plugins/file';
 export * from './plugins/hr';
