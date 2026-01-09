5800aef433c9ab82f075a22224609167bf7174dc
ilimei <hugehardzhang@gmail.com>
2026-01-10 01:48:58 +0800

    ✨ feat: update hotkey priorities to COMMAND_PRIORITY_LOW for improved functionality

--- src/const/hotkey.ts
+++ src/const/hotkey.ts
@@ -1,4 +1,4 @@
-import { COMMAND_PRIORITY_EDITOR } from 'lexical';
+import { COMMAND_PRIORITY_LOW } from 'lexical';
 
 import { HotkeyEnum, HotkeyItem, HotkeyScopeEnum, KeyEnum } from '@/types/hotkey';
 
@@ -10,49 +10,49 @@ export const HOTKEYS_REGISTRATION: HotkeyRegistration = [
   {
     id: HotkeyEnum.Undo,
     keys: combineKeys([KeyEnum.Mod, 'z']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.Redo,
     keys: combineKeys([KeyEnum.Mod, 'y']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.Bold,
     keys: combineKeys([KeyEnum.Mod, 'b']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.Italic,
     keys: combineKeys([KeyEnum.Mod, 'i']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.Underline,
     keys: combineKeys([KeyEnum.Mod, 'u']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.Strikethrough,
     keys: combineKeys([KeyEnum.Mod, KeyEnum.Shift, 'x']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
   {
     id: HotkeyEnum.CodeInline,
     keys: combineKeys([KeyEnum.Mod, 'e']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format, HotkeyScopeEnum.Plugin],
   },
   {
     id: HotkeyEnum.Link,
     keys: combineKeys([KeyEnum.Mod, KeyEnum.Shift, 'k']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format, HotkeyScopeEnum.Plugin],
   },
 
@@ -60,19 +60,19 @@ export const HOTKEYS_REGISTRATION: HotkeyRegistration = [
   {
     id: HotkeyEnum.NumberList,
     keys: combineKeys([KeyEnum.Mod, KeyEnum.Shift, '7']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Insert, HotkeyScopeEnum.Plugin],
   },
   {
     id: HotkeyEnum.BulletList,
     keys: combineKeys([KeyEnum.Mod, KeyEnum.Shift, '8']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Insert, HotkeyScopeEnum.Plugin],
   },
   {
     id: HotkeyEnum.PasteAsPlainText,
     keys: combineKeys([KeyEnum.Mod, KeyEnum.Shift, 'v']),
-    priority: COMMAND_PRIORITY_EDITOR,
+    priority: COMMAND_PRIORITY_LOW,
     scopes: [HotkeyScopeEnum.Format],
   },
 ];
