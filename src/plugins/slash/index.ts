a2b0c3b8fe8f60a6e16c9b6e0d1b725b9c1154fd
Innei <tukon479@gmail.com>
2026-03-19 21:17:50 +0800

    🐛 fix(slash): export ISlashOption type from main package
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/plugins/slash/index.ts
+++ src/plugins/slash/index.ts
@@ -1,3 +1,4 @@
 export * from './plugin';
 export * from './react';
 export type { ISlashMenuOption, ISlashService, SlashOptions } from './service/i-slash-service';
+export type { ISlashOption } from './service/i-slash-service';
